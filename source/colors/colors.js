eb78dba60200b74c681cb8b36e898dbe359b35b5
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-10-26 15:17:07 -0300

    Header Section

---
+++ source/colors/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+    black: '#000000',
+    white: '#ffffff',
+    space: '#1F2041',
+    cyber: '#4B3F72'
+}
+
+export default colors
--- source/views/Home.js
+++ source/views/Home.js
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { BlurView } from 'expo-blur'
+
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons'
+
+import colors from '../colors/colors'
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      
+        {/* Header Section */}
+        <BlurView style={styles.headerWrapper} tint="light" intensity={60}>
+            <Ionicons name='person' size={28} color={colors.black}/>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <FontAwesome5 name='chromecast' size={28} color={colors.black}/>
+                <FontAwesome5 name='search' size={24} color={colors.black} style={{marginLeft: 20}}/>
+            </View>
+        </BlurView>
     </View>
   )
 }
@@ -13,6 +25,18 @@ export default Home
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: colors.cyber
+    },
+
+    headerWrapper: {
+        paddingTop: 60,
+        paddingBottom: 5,
+        paddingHorizontal: 25,
+        borderBottomColor: colors.white,
+        borderBottomWidth: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
     }
 })
